e675416c613e7072539d10e5c433fe6f0a836d62
Harshita Agarwal <81259978+harshitaagrwl15@users.noreply.github.com>
2023-09-28 23:51:42 +0530

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -8,4 +8,5 @@ aws ec2 describe-vpc-endpoints --query 'VpcEndpoints[].{ServiceName:ServiceName,
 aws ec2 describe-vpc-endpoints --query 'VpcEndpoints[].{EndpointID:VpcEndpointId, DNSName:DnsEntries[0].DnsName}' --output table
 
 aws ec2 describe-vpc-endpoints --vpc-endpoint-ids YOUR_ENDPOINT_ID
+aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].[InstanceId, Tags[?Key==`Name`].Value | [0], SecurityGroups[].GroupId]' --output table
 
