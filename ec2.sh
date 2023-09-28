9ea3862acc9e485e99e21f70d988e5ab1974f410
Harshita Agarwal <81259978+harshitaagrwl15@users.noreply.github.com>
2023-09-28 23:38:29 +0530

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -2,4 +2,5 @@ aws ec2 describe-instances --query 'Reservations[].Instances[].{InstanceID:Insta
 
 aws ec2 describe-instances --query 'Reservations[].Instances[].{InstanceID:InstanceId, InstanceName:Tags[?Key==`Name`].Value | [0], SecurityGroups:SecurityGroups[*].{GroupName:GroupName, GroupId:GroupId}}' --output table
 
-aws ec2 describe-vpc-endpoints --query 'VpcEndpoints[*].[VpcEndpointId, ServiceName, DnsEntries[*].DnsName]' --output table
+aws ec2 describe-vpc-endpoints --query 'VpcEndpoints[].{ServiceName:ServiceName, DNSName:DnsEntries[0].DnsName}' --output table
+
