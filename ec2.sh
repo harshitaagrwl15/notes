778b7fe34869fed95917a531ff93048e46fa2689
Harshita Agarwal <81259978+harshitaagrwl15@users.noreply.github.com>
2023-09-28 23:33:54 +0530

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,1 +1,3 @@
 aws ec2 describe-instances --query 'Reservations[].Instances[].{InstanceID:InstanceId, SecurityGroups:SecurityGroups[*].{GroupName:GroupName, GroupId:GroupId}}' --output table
+
+aws ec2 describe-instances --query 'Reservations[].Instances[].{InstanceID:InstanceId, InstanceName:Tags[?Key==`Name`].Value | [0], SecurityGroups:SecurityGroups[*].{GroupName:GroupName, GroupId:GroupId}}' --output table
