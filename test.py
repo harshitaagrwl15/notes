39599029f9572777175958f927be8b6d4f7476b7
Harshita Agarwal <81259978+harshitaagrwl15@users.noreply.github.com>
2024-07-29 09:40:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,45 +1,27 @@
-import boto3
-import json
-import os
+from jinja2 import Template
 
-# Initialize the boto3 clients
-codepipeline_client = boto3.client('codepipeline')
-logs_client = boto3.client('logs')
+# Load the Jinja template file
+with open('provider.jinja') as file:
+    template_content = file.read()
 
+# Create a Jinja2 Template object
+template = Template(template_content)
 
-# Get the pipeline state
-response = codepipeline_client.get_pipeline_state(name=pipeline)
+# Define the values to replace in the template
+context = {
+    'aws_region': 'us-west-2',
+    'aws_profile': 'my-aws-profile',
+    'gcp_project': 'my-gcp-project',
+    'gcp_region': 'us-central1',
+    'azure_subscription_id': 'your-azure-subscription-id',
+    'azure_client_id': 'your-azure-client-id',
+    'azure_client_secret': 'your-azure-client-secret',
+    'azure_tenant_id': 'your-azure-tenant-id'
+}
 
-# Get the latest execution ID
-latest_execution = response['stageStates'][0]['latestExecution']['pipelineExecutionId']
+# Render the template with the context values
+rendered_content = template.render(context)
 
-# Define the log group name
-log_group_name = f'/aws/codepipeline/{pipeline_name}'
-
-# Describe the log streams in the log group
-log_streams = logs_client.describe_log_streams(logGroupName=log_group_name)
-
-# Find the log stream for the latest execution
-log_stream_name = None
-for stream in log_streams['logStreams']:
-    if latest_execution in stream['logStreamName']:
-        log_stream_name = stream['logStreamName']
-        break
-
-if not log_stream_name:
-    print("No log stream found for the latest execution")
-    exit()
-
-# Get the log events from the log stream
-log_events = logs_client.get_log_events(logGroupName=log_group_name, logStreamName=log_stream_name)
-
-# Extract the log messages
-logs = []
-for event in log_events['events']:
-    logs.append(event['message'])
-
-# Write logs to a JSON file
-with open("pipeline_logs.json", "w") as logfile:
-    json.dump(logs, logfile, indent=4)
-
-print("Logs written to pipeline_logs.json")
+# Write the rendered content to the new file (provider.tf)
+with open('provider.tf', 'w') as file:
+    file.write(rendered_content)
